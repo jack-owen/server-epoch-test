8b6b54316567a3de3b0024b45c9434790f1f3a0f
Jack Owen <jack@Jacks-MacBook-Air.local>
2023-10-28 19:06:24 +0100

    Remove starter server

--- server/test/controller.test.js
+++
@@ -1,9 +0,0 @@
-const index = require('../src/index');
-
-describe("/time endpoint", () => {
-  test("return server epoch", async () => {
-    const { body } = await request
-      .get("/time")
-      .expect(200);
-  });
-});
