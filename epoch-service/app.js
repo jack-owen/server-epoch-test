46aecae6ac3ecd6552ce17e71abaf27e022a239c
Jack Owen <jack@Jacks-MacBook-Air.local>
2023-10-28 19:20:17 +0100

    Enable CORS

--- epoch-service/app.js
+++ epoch-service/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
 
 app.use((req, res, next) => {
   const authHeader = req.header('Authorization');
