d1bf100f608d2673e1438f85aff121f0c75660d3
Jack Owen <jack@Jacks-MacBook-Air.local>
2023-10-28 19:25:49 +0100

    Display server epoch

--- client/src/Components/Time/index.tsx
+++ client/src/Components/Time/index.tsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./style.css";
 
 const Time = () => {
+  const [serverEpoch, setServerEpoch] = useState("");
+
+  useEffect(() => {
+    getServerEpoch();
+  }, []);
+
+  const getServerEpoch = async () => {
+    try {
+      const res = await fetch(`http://localhost:3001/time`, {
+        headers: { authorization: 'mysecrettoken' },
+      });
+
+      const text = await res.text();
+
+      setServerEpoch(text);
+    } catch (err) {
+      console.error("Error:", err);
+    }
+  };
+
   return (
     <div className="time">
-        <h1>/time endpoint</h1>
-
-   </div>
+      <h1>/time endpoint</h1>
+      <div>
+        <p>Server Epoch</p>
+        <pre>{serverEpoch}</pre>
+      </div>
+    </div>
   );
 };
+
 
 export default Time;
