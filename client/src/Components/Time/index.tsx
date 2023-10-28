a2c1d8d845c839a7dea8048f5a7d0d2d7911d0a4
Jack Owen <jack@Jacks-MacBook-Air.local>
2023-10-28 19:32:10 +0100

    Simulate 1 second loading effect

--- client/src/Components/Time/index.tsx
+++ client/src/Components/Time/index.tsx
@@ -4,6 +4,7 @@ import "./style.css";
 
 const Time = () => {
   const [serverEpoch, setServerEpoch] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     getServerEpoch();
@@ -11,6 +12,7 @@ const Time = () => {
 
   const getServerEpoch = async () => {
     try {
+      setIsLoading(true);
       const res = await fetch(`http://localhost:3001/time`, {
         headers: { authorization: 'mysecrettoken' },
       });
@@ -18,7 +20,13 @@ const Time = () => {
       const text = await res.text();
 
       setServerEpoch(text);
+      const delay = setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
+  
+      return () => clearTimeout(delay);
     } catch (err) {
+      setIsLoading(false);
       console.error("Error:", err);
     }
   };
@@ -28,7 +36,14 @@ const Time = () => {
       <h1>/time endpoint</h1>
       <div>
         <p>Server Epoch</p>
-        <pre>{serverEpoch}</pre>
+        {isLoading ? 
+          (
+            <div className="loading-spinner">
+              <div className="spinner"></div>
+            </div>
+          ) :
+          <pre>{serverEpoch}</pre>
+        }
       </div>
     </div>
   );
