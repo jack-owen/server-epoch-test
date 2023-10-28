6b1870803849b840208125f5751b6c928f3a6d84
Jack Owen <jack@Jacks-MacBook-Air.local>
2023-10-28 11:55:36 +0100

    Assert mock timestamp

--- epoch-service/test/app.test.js
+++ epoch-service/test/app.test.js
@@ -1,14 +1,19 @@
 const app = require('../app');
 const request = require("supertest")(app);
 
+const mockTimestamp = new Date('2019-04-07T10:20:30Z').getTime();
+
 test('true to be truthy', () => {
   expect(true).toBe(true);
 });
 
 test('return valid time', async () => {
+  global.Date.now = jest.fn(() => mockTimestamp)
+
   const { body } = await request
     .get("/time")
     .expect(200);
 
   expect(body).toEqual({ epoch: expect.any(Number) });
+  expect(body.epoch).toBe(mockTimestamp);
 });
