649a43c3e5ab5065644df408732efb1996352576
Jack Owen <jack@Jacks-MacBook-Air.local>
2023-10-28 12:07:56 +0100

    Add metrics endpoint

--- epoch-service/app.js
+++ epoch-service/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/time');
+var metricsRouter = require('./routes/metrics');
 
 var app = express();
 
@@ -16,5 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/time', usersRouter);
+app.use('/metrics', metricsRouter);
 
 module.exports = app;
