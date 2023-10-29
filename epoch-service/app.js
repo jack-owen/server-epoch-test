0ac6d825453eea709170a1fbc1c4c6cee200cff0
Jack Owen <jack@Jacks-MacBook-Air.local>
2023-10-29 11:56:25 +0000

    Apply formating correction

--- epoch-service/app.js
+++ epoch-service/app.js
@@ -1,38 +1,40 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const promMid = require('express-prometheus-middleware');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const promMid = require("express-prometheus-middleware");
 
-const timeRouter = require('./routes/time');
+const timeRouter = require("./routes/time");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 
 app.use((req, res, next) => {
-  const authHeader = req.header('Authorization');
-  if (authHeader === 'mysecrettoken') {
+  const authHeader = req.header("Authorization");
+  if (authHeader === "mysecrettoken") {
     next();
   } else {
-    res.status(403).json({ error: 'Forbidden: Invalid Authorization token' });
+    res.status(403).json({ error: "Forbidden: Invalid Authorization token" });
   }
 });
 
-app.use('/time', timeRouter);
-app.use(promMid({
-  metricsPath: '/metrics',
-  collectDefaultMetrics: true,
-  requestDurationBuckets: [0.1, 0.5, 1, 1.5],
-  requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
-  responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
-}));
+app.use("/time", timeRouter);
+app.use(
+  promMid({
+    metricsPath: "/metrics",
+    collectDefaultMetrics: true,
+    requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+    requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+    responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+  })
+);
 
 module.exports = app;
--- epoch-service/test/app.test.js
+++ epoch-service/test/app.test.js
@@ -1,53 +1,57 @@
-const app = require('../app');
-const request = require('supertest')(app);
+const app = require("../app");
+const request = require("supertest")(app);
 
-const authToken = 'mysecrettoken';
-const mockTimestamp = new Date('2019-04-07T10:20:30Z').getTime();
+const authToken = "mysecrettoken";
+const mockTimestamp = new Date("2019-04-07T10:20:30Z").getTime();
 
 const defaultMetrics = [
-  'process_cpu_user_seconds_total',
-  'process_cpu_system_seconds_total',
-  'process_cpu_seconds_total',
-  'process_start_time_seconds',
-  'process_resident_memory_bytes',
-  'nodejs_eventloop_lag_seconds',
-  'nodejs_eventloop_lag_min_seconds',
-  'nodejs_eventloop_lag_max_seconds',
-  'nodejs_eventloop_lag_mean_seconds',
-  'nodejs_eventloop_lag_stddev_seconds',
-  'nodejs_eventloop_lag_p50_seconds',
-  'nodejs_eventloop_lag_p90_seconds',
-  'nodejs_eventloop_lag_p99_seconds',
-  'nodejs_active_handles',
-  'nodejs_active_handles_total',
-  'nodejs_active_requests',
-  'nodejs_active_requests_total',
-  'nodejs_heap_size_total_bytes',
-  'nodejs_heap_size_used_bytes',
-  'nodejs_external_memory_bytes',
-  'nodejs_heap_space_size_total_bytes',
-  'nodejs_heap_space_size_used_bytes',
-  'nodejs_heap_space_size_available_bytes',
-  'nodejs_version_info',
-  'nodejs_gc_duration_seconds',
+  "process_cpu_user_seconds_total",
+  "process_cpu_system_seconds_total",
+  "process_cpu_seconds_total",
+  "process_start_time_seconds",
+  "process_resident_memory_bytes",
+  "nodejs_eventloop_lag_seconds",
+  "nodejs_eventloop_lag_min_seconds",
+  "nodejs_eventloop_lag_max_seconds",
+  "nodejs_eventloop_lag_mean_seconds",
+  "nodejs_eventloop_lag_stddev_seconds",
+  "nodejs_eventloop_lag_p50_seconds",
+  "nodejs_eventloop_lag_p90_seconds",
+  "nodejs_eventloop_lag_p99_seconds",
+  "nodejs_active_handles",
+  "nodejs_active_handles_total",
+  "nodejs_active_requests",
+  "nodejs_active_requests_total",
+  "nodejs_heap_size_total_bytes",
+  "nodejs_heap_size_used_bytes",
+  "nodejs_external_memory_bytes",
+  "nodejs_heap_space_size_total_bytes",
+  "nodejs_heap_space_size_used_bytes",
+  "nodejs_heap_space_size_available_bytes",
+  "nodejs_version_info",
+  "nodejs_gc_duration_seconds",
 ];
 
-test('true to be truthy', () => {
+test("true to be truthy", () => {
   expect(true).toBe(true);
 });
 
-test('return valid time', async () => {
+test("return valid time", async () => {
   global.Date.now = jest.fn(() => mockTimestamp);
 
-  const { body } = await request.get('/time').set('Authorization', authToken)
+  const { body } = await request
+    .get("/time")
+    .set("Authorization", authToken)
     .expect(200);
 
   expect(body).toEqual({ epoch: expect.any(Number) });
   expect(body.epoch).toBe(mockTimestamp);
 });
 
-test('return default prometheus metrics', async () => {
-  const { text } = await request.get('/metrics').set('Authorization', authToken)
+test("return default prometheus metrics", async () => {
+  const { text } = await request
+    .get("/metrics")
+    .set("Authorization", authToken)
     .expect(200);
 
   defaultMetrics.forEach((item) => {
@@ -55,17 +59,19 @@ test('return default prometheus metrics', async () => {
   });
 });
 
-describe('Forbidden for invalid token', () => {
-  test('Forbidden missing token', async () => {
-    const { body } = await request.get('/time').expect(403);
+describe("Forbidden for invalid token", () => {
+  test("Forbidden missing token", async () => {
+    const { body } = await request.get("/time").expect(403);
 
-    expect(body.error).toBe('Forbidden: Invalid Authorization token');
+    expect(body.error).toBe("Forbidden: Invalid Authorization token");
   });
 
-  test('Forbidden wrong token', async () => {
-    const { body } = await request.get('/time').set('Authorization', 'sausages')
+  test("Forbidden wrong token", async () => {
+    const { body } = await request
+      .get("/time")
+      .set("Authorization", "sausages")
       .expect(403);
 
-    expect(body.error).toBe('Forbidden: Invalid Authorization token');
+    expect(body.error).toBe("Forbidden: Invalid Authorization token");
   });
 });
