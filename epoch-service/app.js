311b50bba4ca82c1f12b3657b585d45e418bbbf4
Jack Owen <jack@Jacks-MacBook-Air.local>
2023-10-29 12:00:21 +0000

    Remove express html

--- epoch-service/app.js
+++ epoch-service/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const promMid = require("express-prometheus-middleware");
@@ -13,7 +12,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 
